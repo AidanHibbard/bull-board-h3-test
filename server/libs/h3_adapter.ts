6d30ebec1d9e688658e8a64d9c351b050c13f5db
Aidan Hibbard <aidanchibbard@gmail.com>
2024-01-09 15:16:48 -0800

    renderer still fails

--- server/libs/h3_adapter.ts
+++ server/libs/h3_adapter.ts
@@ -7,7 +7,14 @@ import {
   UIConfig,
 } from '@bull-board/api/dist/typings/app';
 import { readFileSync, statSync } from 'fs';
-import { createRouter, eventHandler, getRouterParams, getQuery, serveStatic } from 'h3';
+import {
+  createRouter,
+  eventHandler,
+  getRouterParams,
+  getQuery,
+  serveStatic,
+  createError,
+} from 'h3';
 import ejs from 'ejs';
 
 export const getContentType = (filename?: string) => {
@@ -41,7 +48,11 @@ export const getContentType = (filename?: string) => {
 
 export class H3Adapter implements IServerAdapter {
   private uiHandler = createRouter();
-  private basePath = '/ui';
+  private basePath = '';
+  private entryRoute: AppViewRoute | undefined;
+  private apiRoutes: AppControllerRoute[] | undefined;
+  private statics: { path: string; route: string } | undefined;
+  private errorHandler: ((error: Error) => ControllerHandlerReturnType) | undefined;
   private bullBoardQueues: BullBoardQueues | undefined;
   private viewPath: string | undefined;
   private uiConfig: UIConfig = {};
@@ -52,27 +63,7 @@ export class H3Adapter implements IServerAdapter {
   }
 
   public setStaticPath(staticsRoute: string, staticsPath: string): H3Adapter {
-    const getStaticPath = (relativePath: string) =>
-      `${staticsPath}${relativePath.replace(`${this.basePath}${staticsRoute}`, '')}`;
-
-    this.uiHandler.get(
-      `${this.basePath}${staticsRoute}/**`,
-      eventHandler((event) => {
-        serveStatic(event, {
-          fallthrough: true,
-          indexNames: undefined,
-          getContents: (id) => readFileSync(getStaticPath(id)),
-          getMeta: (id) => {
-            const fileStat = statSync(getStaticPath(id));
-
-            return {
-              size: fileStat.size,
-              type: getContentType(id),
-            };
-          },
-        });
-      })
-    );
+    this.statics = { route: staticsRoute, path: staticsPath };
 
     return this;
   }
@@ -83,49 +74,20 @@ export class H3Adapter implements IServerAdapter {
     return this;
   }
 
-  public setErrorHandler(_handler: (error: Error) => ControllerHandlerReturnType) {
+  public setErrorHandler(handler: (error: Error) => ControllerHandlerReturnType) {
+    this.errorHandler = handler;
+
     return this;
   }
 
   public setApiRoutes(routes: AppControllerRoute[]): H3Adapter {
-    routes.forEach(({ route, handler, method }) => {
-      this.uiHandler.use(
-        `${this.basePath}${route}`,
-        eventHandler((event) => {
-          const { body } = handler({
-            queues: this.bullBoardQueues as BullBoardQueues,
-            params: getRouterParams(event),
-            query: getQuery(event),
-          });
-
-          return body;
-        }),
-        method
-      );
-    });
+    this.apiRoutes = routes;
 
     return this;
   }
 
   public setEntryRoute(routeDef: AppViewRoute): H3Adapter {
-    const { method, route } = routeDef;
-    const routes = Array.isArray(route) ? route : [route];
-
-    routes.forEach((route) => {
-      this.uiHandler.use(
-        `${this.basePath}${route}`,
-        eventHandler(() => {
-          ejs.renderFile(this.viewPath + '/index.ejs', {
-            basePath: `${this.basePath}/`,
-            title: this.uiConfig.boardTitle ?? 'BullMQ',
-            favIconAlternative: this.uiConfig.favIcon?.alternative ?? '',
-            favIconDefault: this.uiConfig.favIcon?.default ?? '',
-            uiConfig: JSON.stringify(this.uiConfig),
-          });
-        }),
-        method
-      );
-    });
+    this.entryRoute = routeDef;
 
     return this;
   }
@@ -142,6 +104,94 @@ export class H3Adapter implements IServerAdapter {
   }
 
   public registerHandlers() {
+    if (!this.statics) {
+      throw new Error(`Please call 'setStaticPath' before using 'registerPlugin'`);
+    } else if (!this.entryRoute) {
+      throw new Error(`Please call 'setEntryRoute' before using 'registerPlugin'`);
+    } else if (!this.viewPath) {
+      throw new Error(`Please call 'setViewsPath' before using 'registerPlugin'`);
+    } else if (!this.apiRoutes) {
+      throw new Error(`Please call 'setApiRoutes' before using 'registerPlugin'`);
+    } else if (!this.bullBoardQueues) {
+      throw new Error(`Please call 'setQueues' before using 'registerPlugin'`);
+    } else if (!this.errorHandler) {
+      throw new Error(`Please call 'setErrorHandler' before using 'registerPlugin'`);
+    }
+
+    const getStaticPath = (relativePath: string) => {
+      if (!this.statics) return '';
+
+      return `${this.statics.path}${relativePath.replace(
+        `${this.basePath}${this.statics.route}`,
+        ''
+      )}`;
+    };
+
+    const { method, route, handler } = this.entryRoute;
+
+    const routes = Array.isArray(route) ? route : [route];
+
+    routes.forEach((route) => {
+      this.uiHandler.use(
+        `${this.basePath}${route}`,
+        eventHandler(async () => {
+          const { name: filename, params } = handler({
+            basePath: this.basePath,
+            uiConfig: this.uiConfig,
+          });
+
+          return ejs.renderFile(`${this.viewPath}/${filename}`, params);
+        }),
+        method
+      );
+    });
+
+    this.uiHandler.get(
+      `${this.basePath}${this.statics.route}/**`,
+      eventHandler(async (event) => {
+        await serveStatic(event, {
+          fallthrough: true,
+          indexNames: undefined,
+          getContents: (id) => readFileSync(getStaticPath(id)),
+          getMeta: (id) => {
+            const fileStat = statSync(getStaticPath(id));
+
+            return {
+              size: fileStat.size,
+              type: getContentType(id),
+            };
+          },
+        });
+      })
+    );
+
+    this.apiRoutes.forEach(({ route, handler, method }) => {
+      this.uiHandler.use(
+        `${this.basePath}${route}`,
+        eventHandler(async (event) => {
+          try {
+            const { body } = await handler({
+              queues: this.bullBoardQueues as BullBoardQueues,
+              params: getRouterParams(event),
+              query: getQuery(event),
+            });
+
+            return body;
+          } catch (e) {
+            if (this.errorHandler) {
+              const { body, status } = this.errorHandler(e as Error);
+
+              return createError({
+                statusCode: status,
+                data: body,
+              });
+            }
+          }
+        }),
+        method
+      );
+    });
+
     return this.uiHandler;
   }
 }
